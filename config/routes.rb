52c84d7a1d80c2da1ae8ce1c6bbaac1954c5d71b
AJ Keeter <aleckeeter94@gmail.com>
2019-06-08 15:01:06 -0500

    Adds Static Pages, Controller, Routes, Navigation, and Views to the Application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+
+  get 'pages/terms'
+
+  get 'pages/errors'
+
+  root to: 'pages#home'
+
+  match 'home', to: 'pages#home', via: 'get'
+  match 'terms', to: 'pages#terms', via: 'get'
+  match 'conditions', to: 'pages#terms', via: 'get'
+  match 'errors', to: 'pages#errors', via: 'get'
+
+
+  get '*path', to: redirect('/errors')
 end
